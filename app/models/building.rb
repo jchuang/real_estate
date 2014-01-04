7e3dad9d563a0252dd992e69f251b860406fe00e
Janet Chuang <janet.chuang@gmail.com>
2014-01-04 17:19:34 -0500

    validate presence and format of building attributes

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,16 @@
 class Building < ActiveRecord::Base
+
+  # 50 US states, plus District of Columbia
+
+  STATE_ABBREVIATIONS = %w(AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA
+    KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI
+    SC SD TN TX UT VT VA WA WV WI WY)
+
+  validates :address, presence: true, format: { with: /\A[0-9]+\s[a-zA-Z]+/,
+    message: 'must include number and street name' }
+  validates :city, presence: true
+  validates :state, presence: true, inclusion: { in: STATE_ABBREVIATIONS }
+  validates :zip_code, presence: true, format: { with: /\A\d{5}\z/,
+    message: 'must be 5 digits long' }
+
 end
--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Building do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:blanks) { [nil, ''] }
+
+  it { should have_valid(:address).when('33 Harrison Ave.', '33 Harrison') }
+  it { should_not have_valid(:address).when('Harrison Ave', '33') }
+  it { should_not have_valid(:address).when(*blanks) }
+
+  it { should have_valid(:city).when('Boston', 'Las Vegas') }
+  it { should_not have_valid(:city).when(*blanks) }
+
+  it { should have_valid(:state).when('MA', 'NY') }
+  it { should_not have_valid(:state).when('Mass', 'Massachusetts') }
+  it { should_not have_valid(:state).when(*blanks) }
+
+  it { should have_valid(:zip_code).when('02111', '91789') }
+  it { should_not have_valid(:zip_code).when('02111-1234', 'H2V 1X9') }
+  it { should_not have_valid(:zip_code).when(*blanks) }
+
 end
