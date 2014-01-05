83f1400910948ffb6744e012e4ae36b93244050c
Janet Chuang <janet.chuang@gmail.com>
2014-01-04 21:55:10 -0500

    write acceptance test for associating owner with buildings

--- spec/features/associate_building_with_owner_spec.rb
+++ spec/features/associate_building_with_owner_spec.rb
@@ -11,7 +11,21 @@ feature 'associate building with owner', %q{
   # When recording a building, I want to optionally associate the building with its rightful owner.
   # If I delete an owner, the owner and its primary key should no longer be associated with any properties.
 
-  scenario 'assign an owner when creating a building'
+  scenario 'assign an owner when creating a building' do
+    owner1 = FactoryGirl.create(:owner)
+    owner2 = FactoryGirl.create(:owner)
+
+    building1 = FactoryGirl.create(:building, owner: owner1)
+    building2 = FactoryGirl.create(:building, owner: owner1)
+    building3 = FactoryGirl.create(:building, owner: nil)
+
+    visit "owners/#{owner1.id}"
+    expect(page).to have_content building1.address
+    expect(page).to have_content building2.address
+
+    visit "owners/#{owner2.id}"
+    expect(page).to_not have_content 'Boston'
+  end
 
   scenario 'deleting an owner means their properties are no longer associated'
 
