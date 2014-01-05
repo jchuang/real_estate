f84102901ed48383434f81d4a05aaa116d6db6ba
Janet Chuang <janet.chuang@gmail.com>
2014-01-04 20:53:29 -0500

    validate owner attributes, including email format

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,8 @@
 class Owner < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ }
+
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Owner do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:blanks) { [nil, ''] }
+
+  it { should have_valid(:first_name).when('Sally', 'Neil') }
+  it { should_not have_valid(:first_name).when(*blanks) }
+
+  it { should have_valid(:last_name).when('Ride', 'Armstrong') }
+  it { should_not have_valid(:last_name).when(*blanks) }
+
+  it { should have_valid(:email).when('sally@ride.com',
+    'neil.armstrong@gmail.com') }
+  it { should_not have_valid(:email).when('sally@ride', '@gmail.com',
+    'neil.armstrong') }
+  it { should_not have_valid(:email).when(*blanks) }
+
 end
